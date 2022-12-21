cfa321076a08beb84db69194d7b75dece0f9e611
Afr Schoe <58883403+q9f@users.noreply.github.com>
2022-12-21 15:36:52 +0100

    eth/chain: add documentation for eip 155 cases

--- src/eth/chain.cr
+++ src/eth/chain.cr
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Encapsulates `Eth::Chain` IDs and utilities for EIP-155 compatibility.
+#
+# Ref: <https://eips.ethereum.org/EIPS/eip-155>
 module Eth::Chain
   extend self
 
@@ -90,14 +93,48 @@ module Eth::Chain
   # Chain ID for the geth private network preset.
   PRIVATE_GETH = 1337
 
+  # Checks if a provided `v` belongs to a Ledger wallet signature without
+  # EIP-155 replay protection.
+  #
+  # Parameters:
+  # * `v` (`Int32`): the signature's `v` value.
+  #
+  # ```
+  # Chain.ledger? 0
+  # # => true
+  # ```
   def ledger?(v : Int32)
     0 === v || 1 === v
   end
 
+  # Checks if a provided `v` belongs to a legacy signature without
+  # EIP-155 replay protection.
+  #
+  # Parameters:
+  # * `v` (`Int32`): the signature's `v` value.
+  #
+  # ```
+  # Chain.legacy? 27
+  # # => true
+  # ```
   def legacy?(v : Int32)
     27 === v || 28 === v
   end
 
+  # Converts a provided `v` value to an ECDSA recovery ID for the
+  # given EIP-155 chain ID.
+  #
+  # Parameters:
+  # * `v` (`Int32`): the signature's `v` value.
+  # * `chain_id` (`Int32`): the chain ID the signatrue was generated on
+  #   (default: `ETHEREUM`).
+  #
+  # Raises an exception if the `v` value does not match the chain ID.
+  #
+  # ```
+  # Chain.to_recovery_id 38
+  # # => 1
+  # ```
   def to_recovery_id(v : Int32, chain_id : Int32 = ETHEREUM)
     e : Int32 = 0 + 2 * chain_id + 35
     i : Int32 = 1 + 2 * chain_id + 35
@@ -115,10 +152,31 @@ module Eth::Chain
     end
   end
 
+  # Converts a provided recovery ID to the according `v` for legacy
+  # signatures without EIP-155 replay protection.
+  #
+  # Parameters:
+  # * `recovery_id` (`Int32`): the recovery ID.
+  #
+  # ```
+  # Chain.to_v 1
+  # # => 28
+  # ```
   def to_v(recovery_id : Int32)
     return 27 + recovery_id
   end
 
+  # Converts a provided recovery ID to the according `v` for signatures
+  # with EIP-155 replay protection.
+  #
+  # Parameters:
+  # * `recovery_id` (`Int32`): the recovery ID.
+  # * `chain_id` (`Int32`): the chain ID the signatrue was generated on.
+  #
+  # ```
+  # Chain.to_v(1, Chain::ETHEREUM)
+  # # => 38
+  # ```
   def to_v(recovery_id : Int32, chain_id : Int32)
     if chain_id < 1
       return to_v(recovery_id)
@@ -127,6 +185,17 @@ module Eth::Chain
     end
   end
 
+  # Converts a provided `v` value to an EIP-155 chain ID. This does not
+  # work for legacy signatures with `v < 36` and return `nil` in that
+  # case.
+  #
+  # Parameters:
+  # * `v` (`Int32`): the signature's `v` value.
+  #
+  # ```
+  # Chain.to_chain_id 38
+  # # => 1
+  # ```
   def to_chain_id(v : Int32)
     return nil if v < 36
     chain_id = (v - 35) // 2
